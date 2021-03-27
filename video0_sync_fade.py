8bd9ca58c361dc1d1c92007df125215a93ddc367
luphord <luphord@protonmail.com>
2021-03-27 10:24:59 +0100

    shift audio

--- video0_sync_fade.py
+++ video0_sync_fade.py
@@ -10,5 +10,6 @@ if len(argv) < 2:
 in_file = Path(argv[1])
 out_file = in_file.parent / f"{in_file.stem}-fade-sync{in_file.suffix}"
 
-video = VideoFileClip(str(in_file)).fadein(3).fadeout(3).audio_fadein(3).audio_fadeout(3)
+video = VideoFileClip(str(in_file)).fadein(3).fadeout(3)
+video.audio = video.audio.subclip(t_start=0.3, t_end=video.duration).audio_fadein(3).audio_fadeout(3)
 video.write_videofile(str(out_file), bitrate="2M")
