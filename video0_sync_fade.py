5f9eaf595e111ebc0a25a3e7d7793b05d036e22e
luphord <luphord@protonmail.com>
2021-03-27 09:51:41 +0100

    add first version of video8 sync + fade helper

---
+++ video0_sync_fade.py
@@ -0,0 +1,14 @@
+from sys import argv
+from pathlib import Path
+from moviepy.editor import *
+from moviepy import *
+
+if len(argv) < 2:
+    print(f"Usage: {argv[0]} /path/to/video.mp4")
+    sys.exit(1)
+
+in_file = Path(argv[1])
+out_file = in_file.parent / f"{in_file.stem}-fade-sync{in_file.suffix}"
+
+video = VideoFileClip(str(in_file)).fadein(3).fadeout(3).audio_fadein(3).audio_fadeout(3)
+video.write_videofile(str(out_file))
